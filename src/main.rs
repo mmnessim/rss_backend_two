3d896b6ac2f43f6f4185c330849a2e1206d61800
Mounir Nessim <mmnessim@gmail.com>
2026-01-04 22:14:10 -0500

    add serde_json dependency and implement file watcher for feeds.json

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::path::Path;
+use std::{fs, path::Path, str};
 
 use notify::{Event, RecursiveMode, Watcher};
+use serde::Deserialize;
 use tracing::Level;
 use tracing_subscriber::EnvFilter;
 
@@ -16,30 +17,7 @@ async fn main() {
         tracing::error!("panic: {}", panic_info);
     }));
 
-    tokio::spawn(async {
-        let (tx, mut rx) = tokio::sync::mpsc::channel::<notify::Result<Event>>(100);
-
-        let mut watcher = match notify::recommended_watcher(move |res| {
-            let _ = tx.blocking_send(res);
-        }) {
-            Ok(w) => w,
-            Err(e) => {
-                tracing::error!("Error creating watcher: {e}");
-                return;
-            }
-        };
-
-        watcher
-            .watch(Path::new("."), RecursiveMode::NonRecursive)
-            .unwrap();
-
-        while let Some(res) = rx.recv().await {
-            match res {
-                Ok(event) => tracing::info!("event: {:?} {:?}", event.kind, event.paths),
-                Err(e) => tracing::error!("watch error: {:?}", e),
-            }
-        }
-    });
+    tokio::spawn(async { file_watcher().await });
 
     let app = routes::router();
 
@@ -55,4 +33,66 @@ async fn main() {
 
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
     let _ = axum::serve(listener, app).await;
+}
+
+async fn file_watcher() {
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<notify::Result<Event>>(100);
+
+    let mut watcher = match notify::recommended_watcher(move |res| {
+        let _ = tx.blocking_send(res);
+    }) {
+        Ok(w) => w,
+        Err(e) => {
+            tracing::error!("Error creating watcher: {e}");
+            return;
+        }
+    };
+
+    watcher
+        .watch(Path::new("./feeds.json"), RecursiveMode::NonRecursive)
+        .unwrap();
+
+    while let Some(res) = rx.recv().await {
+        match res {
+            Ok(event) => {
+                read_file();
+                tracing::info!("event: {:?} {:?}", event.kind, event.paths)
+            }
+            Err(e) => tracing::error!("watch error: {:?}", e),
+        }
+    }
+}
+
+fn read_file() {
+    let bytes = match fs::read("./feeds.json") {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            tracing::error!("Error reading feeds.json {:?}", e);
+            return;
+        }
+    };
+    match str::from_utf8(&bytes) {
+        Ok(s) => println!("{s}"),
+        Err(e) => println!("Error: {:?}", e),
+    }
+
+    let feeds_opt = match serde_json::from_slice::<Vec<Feed>>(&bytes) {
+        Ok(feeds) => {
+            tracing::info!("Loaded {} feeds", feeds.len());
+            Some(feeds)
+        }
+        Err(e) => {
+            tracing::error!("Error parsing feeds from feeds.json: {:?}", e);
+            None
+        }
+    };
+    if let Some(feeds) = feeds_opt {
+        println!("{} {}", feeds[0].source, feeds[0].url)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Feed {
+    source: String,
+    url: String,
 }
